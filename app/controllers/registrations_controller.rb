24f96e10fcdb3fb872e6111f197648f79d3955c7
yuyaamano23 <nba.chako.nba@icloud.com>
2020-05-15 18:39:44 +0000

    プロフィール編集後のリダイレクト先を変更

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,9 @@ class RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_current_password(params)
   end
+  
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
+  
 end
