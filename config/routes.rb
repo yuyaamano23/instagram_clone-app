2be74b0e928773a9306dab1bf8ede7686e2ca627
yuyaamano23 <nba.chako.nba@icloud.com>
2020-05-16 17:09:06 +0000

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get '/users/:id', to: 'users#show', as: 'user' 
+  
+  get '/posts/new', to: 'posts#new'
+  post '/posts', to: 'posts#create'
+  post '/posts/:post_id/photos', to: 'photos#create', as: 'post_photos'
 end
