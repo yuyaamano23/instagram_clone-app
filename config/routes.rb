88ac7bd06efd67be2c68cb64672d48986d8f2de3
yuyaamano23 <nba.chako.nba@icloud.com>
2020-05-16 17:13:00 +0000

    resourcesメソッドによるルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/users/:id', to: 'users#show', as: 'user' 
   
-  get '/posts/new', to: 'posts#new'
-  post '/posts', to: 'posts#create'
-  post '/posts/:post_id/photos', to: 'photos#create', as: 'post_photos'
+  resources :posts, only: %i(new create) do
+    resources :photos, only: %i(create)
+  end  
 end
