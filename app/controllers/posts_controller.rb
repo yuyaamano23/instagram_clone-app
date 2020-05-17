55ec46949250383a0ad867654f92a04997283655
yuyaamano23 <nba.chako.nba@icloud.com>
2020-05-17 16:33:39 +0000

    postsコントローラーのdestroyアクション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,16 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
 
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    if @post.user == current_user
+      flash[:notice] = "投稿が削除されました" if @post.destroy
+    else
+      flash[:alert] = "投稿の削除に失敗しました"
+    end
+    redirect_to root_path
+  end
+  
   private
     def post_params
       params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
