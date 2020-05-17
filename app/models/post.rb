880af93c8a42c8bb4deea0f3d47bc056fae7db71
yuyaamano23 <nba.chako.nba@icloud.com>
2020-05-17 18:17:18 +0000

    記事一覧からのいいね機能を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ApplicationRecord
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
 
   accepts_nested_attributes_for :photos
+  
+  def liked_by(user)
+    # user_idとpost_idが一致するlikeを検索する
+    Like.find_by(user_id: user.id, post_id: id)
+  end
 end
