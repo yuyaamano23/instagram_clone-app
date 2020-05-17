8eece7cca22ff29847209ee9f85980f60bd98c59
yuyaamano23 <nba.chako.nba@icloud.com>
2020-05-17 17:21:19 +0000

    投稿削除の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   
+  # before_action :set_post, only: %i(show destroy)
+  # リファクタリングするとエラーが起こるのでこの行は封印します
+  
   def new
     @post = Post.new
     @post.photos.build
