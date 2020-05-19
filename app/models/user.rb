588413c9c40f7e8d93607435cd29fc788c7bbe14
yuyaamano23 <nba.chako.nba@icloud.com>
2020-05-19 15:58:11 +0000

    アソシエーション®設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   
   has_many :posts, dependent: :destroy
   has_many :likes
+  
+  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
