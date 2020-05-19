eb258147e7f9b4c8e3bc8b4ec523f427ff1936a7
yuyaamano23 <nba.chako.nba@icloud.com>
2020-05-19 15:52:56 +0000

    Commentモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_174437) do
+ActiveRecord::Schema.define(version: 2020_05_19_155135) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment", null: false
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "post_id", null: false
